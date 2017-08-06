d2b4f9dec18e91915fa769373a21f17259a3ec42
Partha Mahajani <partha.mahajani@appdirect.com>
2017-08-05 19:54:18 -0700

    Updating package declaration for config classes

--- src/main/java/config/Application.java
+++ src/main/java/config/Application.java
@@ -1,14 +1,17 @@
-package hello;
+package config;
 
 import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
@@ -26,8 +29,6 @@ public class Application {
             for (String beanName : beanNames) {
                 System.out.println(beanName);
             }
-
         };
     }
-
 }
--- src/main/java/config/HelloController.java
+++ src/main/java/config/HelloController.java
@@ -1,11 +1,10 @@
-package hello;
+package config;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!\n";
